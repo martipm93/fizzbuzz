6e6e7de39d019a547a22bdebc8f8d5efc3a3be92
Marti Pons <marti_pm93@hotmail.com>
2019-10-01 21:09:54 +0200

    FizzBuzz model

--- src/main/java/com/concatel/exam1/persistence/model/FizzBuzz.java
+++ src/main/java/com/concatel/exam1/persistence/model/FizzBuzz.java
@@ -1,4 +1,41 @@
 package com.concatel.exam1.persistence.model;
 
+import java.util.Objects;
+
 public class FizzBuzz {
+
+    private String fizzBuzzString;
+
+    public FizzBuzz(final String fizzBuzzString) {
+        this.fizzBuzzString = fizzBuzzString;
+    }
+
+    public String getFizzBuzzString() {
+        return fizzBuzzString;
+    }
+
+    public void setFizzBuzzString(String fizzBuzzString) {
+        this.fizzBuzzString = fizzBuzzString;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FizzBuzz fizzBuzz = (FizzBuzz) o;
+        return Objects.equals(fizzBuzzString, fizzBuzz.fizzBuzzString);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(fizzBuzzString);
+    }
+
+    @Override
+    public String toString() {
+        return "FizzBuzz{" +
+                "fizzBuzzString='" + fizzBuzzString + '\'' +
+                '}';
+    }
 }
